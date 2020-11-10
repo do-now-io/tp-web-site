13d382eb93249dbeb1a9cf3ea730df0380dd33e5
Baptista <julien.agen@gmail.com>
2020-11-10 10:27:42 +0100

    voila-mon-exo

---
+++ AppCenter/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AppareilComponent } from './appareil/appareil.component';
+import { MonPremierComponent } from './mon-premier/mon-premier.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MonPremierComponent,
+    AppareilComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
